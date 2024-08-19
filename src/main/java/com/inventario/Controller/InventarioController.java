f2528e4e67fc8085ba590d9e54bad49f069002e3
Jesus Camacaro <davidjcamacaro@gmail.com>
2024-08-19 12:32:06 -0400

    19-08-24

--- src/main/java/com/inventario/Controller/InventarioController.java
+++ src/main/java/com/inventario/Controller/InventarioController.java
@@ -1,39 +1,43 @@
 package com.inventario.Controller;
 
-
+import java.util.Optional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import com.inventario.Model.ModeloInventario;
 import com.inventario.Services.ServicesInventario;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/inventario")
 public class InventarioController {
 
-    final
-    ServicesInventario servicesInventario;
+    final ServicesInventario servicesInventario;
+
     public InventarioController(ServicesInventario servicesInventario) {
         this.servicesInventario = servicesInventario;
     }
 
-
     @PostMapping
-    public ModeloInventario createInv(@RequestBody ModeloInventario modeloInventario){
+    public ModeloInventario createInv(@RequestBody ModeloInventario modeloInventario) {
         return servicesInventario.createId(modeloInventario);
     }
 
     @GetMapping
-    public ModeloInventario getAll(){
+    public ModeloInventario getAll() {
         return (ModeloInventario) servicesInventario.findAll();
     }
 
     @GetMapping(path = "{id}")
-    public ModeloInventario getById(@PathVariable(name = "id") Integer id){
+    public Optional<ModeloInventario> getById(@PathVariable(name = "id") Integer id) {
         return servicesInventario.findById(id);
     }
 
     @DeleteMapping(path = "{id}")
-    public void deleteById(@PathVariable(name = "id") Integer id){
+    public void deleteById(@PathVariable(name = "id") Integer id) {
         servicesInventario.deleteById(id);
     }
 }
--- src/main/java/com/inventario/Services/ServicesInventario.java
+++ src/main/java/com/inventario/Services/ServicesInventario.java
@@ -1,38 +1,33 @@
 package com.inventario.Services;
 
-import com.inventario.Model.ModeloInventario;
-import com.inventario.Repository.RepoInventario;
-import jakarta.persistence.Id;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.stereotype.Service;
+import com.inventario.Model.ModeloInventario;
+import com.inventario.Repository.RepoInventario;
 
 @Service
 public class ServicesInventario {
 
-    final
-    RepoInventario repoInventario;
-
+    final RepoInventario repoInventario;
 
     public ServicesInventario(RepoInventario repoInventario) {
         this.repoInventario = repoInventario;
-
     }
 
-    public ModeloInventario createId(ModeloInventario modeloInventario){
+    public ModeloInventario createId(ModeloInventario modeloInventario) {
         return repoInventario.save(modeloInventario);
     }
 
-    public List<ModeloInventario> findAll(){
+    public List<ModeloInventario> findAll() {
         return repoInventario.findAll();
     }
 
-    public ModeloInventario findById(Integer id){
-        return repoInventario.getReferenceById(id);
+    public Optional<ModeloInventario> findById(Integer id) {
+        return repoInventario.findById(id);
     }
-    public void deleteById(Integer id){
+
+    public void deleteById(Integer id) {
         repoInventario.deleteById(id);
     }
 
